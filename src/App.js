5f4171830d9625f39fc5784a3c4c7246c650153a
Rahul-7368 <kuarrahul.godda@gmail.com>
2024-06-20 21:22:01 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+// import App from './App.js'
 import './App.css';
+  function App() {
+  const[members, setMembers] = useState([]);
+  const[currentPage, setCurrentPage] = useState(1);
+  const[error, setError] = useState(null);
+  const rowsPerPage = 10;
 
-function App() {
-  return (
-    <div className="App">
+  useEffect(() => {
+    fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+    .then(response => {
+      if(!response.ok){
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => setMembers(data))
+    .catch(error => {
+      setError(error);
+      alert('Failed to fetch data')
+    });
+  }, []);
+
+  const handlePrevPage = () => {
+    if(currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if(currentPage * rowsPerPage < members.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const displayTable = () => {
+    const start = (currentPage - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+    const paginatedItems = members.slice(start, end);
+
+    return paginatedItems.map(member => (
+      <tr key={member.id}>
+        <td>{member.id}</td>
+        <td>{member.name}</td>
+        <td>{member.email}</td>
+        <td>{member.role}</td>
+      </tr>
+    ));
+  };
+
+  return(
+     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Employee Data Table</h1>
       </header>
-    </div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayTable()}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick = {handlePrevPage}>Previous</button>
+        <button >{currentPage}</button>
+        <button onClick = {handleNextPage} disabled = {currentPage * rowsPerPage >= members.length}>Next</button>
+      </div>
+     </div>
   );
-}
+};
 
 export default App;
